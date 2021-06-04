2e2f96e78f937be0c1794df13e346e1a01cf9bae
Agustin Mediotti <70532867+Agustin-Mediotti@users.noreply.github.com>
2021-06-03 21:11:44 -0300

    first-poem

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import Typical from 'react-typical'
 
 function App() {
+
+  const refreshPage = ()=>{
+    window.location.reload();
+ }
+  
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+      <h1>Sintropía</h1>
+      <Typical
+        loop={1}
+        wrapper='p'
+        steps={[
+          'Fue en aquella playa',
+          1000,
+          'en la que te presentaste como el aire',
+          550,
+          'fue en aquel balcón',
+          550,
+          'en el que te tomé del cuello y mordí tus labios',
+          550,
+          'fue en aquel otoño frío',
+          550,
+          'cuando transgredimos el equilibrio entrópico del universo',
+          550,
+          'fue aquella noche',
+          550,
+          'en la que me acompañaste en soledad',
+          550,
+          'y fue esa mañana',
+          550,
+          'en la que revelaste tu cuerpo desnudo',
+          550,
+          'fue en esa plaza',
+          550,
+          'donde dejamos un rastro de deseo, estampado con calor.',
+          350
+        ]}
+      />
+      </div>
+      <div className='btn' onClick={refreshPage}><span>Leer de nuevo</span></div>
     </div>
+    </>
   );
 }
 
